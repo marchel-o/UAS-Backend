804b10eab9a997e44bda44fc8f9c2741d09e7d5e
marchel-o <marchellinus.535250182@stu.untar.ac.id>
2026-06-13 18:47:51 +0700

    Menambahkan MVC Profile
    User sekarang bisa mengganti nama, email, dan password dengan syarat-syarat tertentu.

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return redirect()->route('tickets.index');
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [UserController::class, 'showLogin'])->name('login');
-    Route::post('login', [UserController::class, 'login']);
-    Route::get('register', [UserController::class, 'showRegister'])->name('register');
-    Route::post('register', [UserController::class, 'register']);
+    Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('register', [AuthController::class, 'register']);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::post('logout', [UserController::class, 'logout'])->name('logout');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     
     Route::resource('tickets', TicketController::class);
     Route::post('tickets/{ticket}/comments', [CommentController::class, 'store'])->name('comments.store');
-
+    
     Route::resource('categories', CategoryController::class)->only(['index', 'create', 'store']);
+
+    Route::resource('profile', ProfileController::class);
+    Route::put('/profile/edit-value', [ProfileController::class, 'editValue'])->name('profile.editValue');
 });
